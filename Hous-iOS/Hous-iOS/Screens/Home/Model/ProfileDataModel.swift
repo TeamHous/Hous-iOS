90ef0cabfd04b49d5bb31e7c16da694abf81d2c3
Kim Minjae <ffalswo2@naver.com>
2022-07-09 20:10:50 +0900

    [#10] ADD : DataModel, 더미데이터 작성
    - 이벤트 DataModel
    - 프로필 DataModel
    - 규칙 DataModel
    - Todo DataModel
    - 더미데이터 추가

--- Hous-iOS/Hous-iOS/Screens/Home/Model/ProfileDataModel.swift
+++ Hous-iOS/Hous-iOS/Screens/Home/Model/ProfileDataModel.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+struct ProfileDataModel {
+    let profileName: String
+    let isTested: Bool
+}
+
+
+extension ProfileDataModel {
+    static let sampleData: [ProfileDataModel] = [
+        ProfileDataModel(profileName: "김민재", isTested: false),
+        ProfileDataModel(profileName: "김지현", isTested: false),
+        ProfileDataModel(profileName: "김호세", isTested: true),
+        ProfileDataModel(profileName: "이의진", isTested: true),
+    ]
+}
--- Hous-iOS/Hous-iOS/Screens/Home/Model/TodoDataModel.swift
+++ Hous-iOS/Hous-iOS/Screens/Home/Model/TodoDataModel.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+struct TodoDataModel {
+    let todoString: String
+    let isDone: Bool
+}
+
+
+extension TodoDataModel {
+    static let sampleData: [TodoDataModel] = [
+        TodoDataModel(todoString: "다영언니랑 마트", isDone: false),
+        TodoDataModel(todoString: "거실 청소기 돌리기", isDone: false)
+//        TodoDataModel(todoString: "저녁 설거지", isDone: false)
+//        TodoDataModel(todoString: "야식 설거지", isDone: true)
+//        TodoDataModel(todoString: "급하게 산 물 청구", isDone: true)
+    ]
+}
