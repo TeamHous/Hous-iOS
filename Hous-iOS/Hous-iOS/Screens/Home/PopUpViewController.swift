6a5971edcccca438b4806ab30fa7abc8e3bdc5c6
Kim Minjae <ffalswo2@naver.com>
2022-07-09 20:24:17 +0900

    [#10] ADD : 이벤트 클릭시 뜨는 팝업창 ViewController 추가

--- Hous-iOS/Hous-iOS/Screens/Home/PopUpViewController.swift
+++ Hous-iOS/Hous-iOS/Screens/Home/PopUpViewController.swift
@@ -6,24 +6,108 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 class PopUpViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    private lazy var blurView = UIVisualEffectView().then {
+        let blurEffect = UIBlurEffect(style: UIBlurEffect.Style.dark)
+        $0.effect = blurEffect
+        $0.frame = view.bounds
+        $0.autoresizingMask = [.flexibleWidth, .flexibleHeight]
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private let popUpView = UIView().then {
+        $0.backgroundColor = .white
+        $0.layer.cornerRadius = 20
     }
-    */
+    
+    private let titleLabel = UILabel().then {
+        $0.text = "이벤트"
+        $0.font = .systemFont(ofSize: 20, weight: .semibold)
+        $0.tintColor = .black
+    }
+    
+    private let eventImageView = UIImageView().then {
+        $0.layer.cornerRadius = 30
+        $0.clipsToBounds = true
+        $0.backgroundColor = UIColor(hex: "FFD66D")
+    }
+    
+    private let eventTextField = UITextField().then {
+        $0.backgroundColor = UIColor(hex: "FFD66D")
+    }
+    
+    private let cancelButton = UIButton().then {
+        var config = UIButton.Configuration.plain()
+        config.title = "취소"
+        config.baseForegroundColor = .red
+        config.baseBackgroundColor = .white
+        $0.configuration = config
+        
+        $0.addTarget(self, action: #selector(cancelButtonDidTapped), for: .touchUpInside)
+        
+        $0.layer.borderColor = UIColor.red.cgColor
+        $0.layer.borderWidth = 1
+    }
+    
+    private let saveButton = UIButton().then {
+        $0.setTitle("저장", for: .normal)
+        $0.backgroundColor = UIColor(hex: "FFD66D")
+    }
+    
+    private lazy var buttonStackView = UIStackView(arrangedSubviews: [cancelButton, saveButton]).then {
+        $0.axis = .horizontal
+        $0.distribution = .fillEqually
+        $0.spacing = 15
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        render()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        
+        if let touch = touches.first, touch.view == self.blurView {
+            dismiss(animated: true)
+        }
+    }
+
+    @objc private func cancelButtonDidTapped() {
+        self.dismiss(animated: true)
+    }
+        
+    private func render() {
+        self.view.addSubview(blurView)
+        self.view.addSubview(popUpView)
+        popUpView.addSubViews([titleLabel, eventImageView, buttonStackView])
+        
+        popUpView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.width.equalTo(UIScreen.main.bounds.width * (272/375))
+            make.height.equalTo(UIScreen.main.bounds.height * (439/812))
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(36)
+            make.leading.equalToSuperview().offset(24)
+        }
+        
+        eventImageView.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(10)
+            make.leading.equalToSuperview().offset(20)
+            make.width.height.equalTo(60)
+        }
+        
+        buttonStackView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(24)
+            make.bottom.equalToSuperview().inset(20)
+        }
+        
+    }
 
 }
+
