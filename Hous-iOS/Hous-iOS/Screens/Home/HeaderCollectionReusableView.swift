7b9c685e2f243473309fe6eabbe5d813e96136d7
Kim Minjae <ffalswo2@naver.com>
2022-07-09 20:17:51 +0900

    [#10] ADD : Section Header에 재사용될 뷰 추가

--- Hous-iOS/Hous-iOS/Screens/Home/HeaderCollectionReusableView.swift
+++ Hous-iOS/Hous-iOS/Screens/Home/HeaderCollectionReusableView.swift
@@ -8,5 +8,33 @@
 import UIKit
 
 class HeaderCollectionReusableView: UICollectionReusableView {
-        
+    
+    static let identifier = "HeaderCollectionReusableView"
+    
+    let subtitleLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 20, weight: .semibold)
+        $0.textAlignment = .left
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        render()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func render() {
+        addSubview(subtitleLabel)
+        subtitleLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(24)
+            make.top.trailing.bottom.equalToSuperview()
+        }
+    }
+    
+    func setSubTitleLabel(data: String) {
+        subtitleLabel.text = data
+    }
+    
 }
