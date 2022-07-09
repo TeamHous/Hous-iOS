8b02bd3b5822476b152eb3696e604f0feedd3a83
Jihyun247 <wlguslove12345@gmail.com>
2022-07-09 15:32:06 +0900

    [#8] FEAT : Rules탭 홈뷰 카테고리 CVC

---
+++ Hous-iOS/Hous-iOS/Screens/Rules/Cell/CategoryCollectionViewCell.swift
@@ -0,0 +1,44 @@
+//
+//  CategoryCollectionViewCell.swift
+//  Hous-iOS
+//
+//  Created by 김지현 on 2022/07/09.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "CategoryCollectionViewCell"
+    
+    var categoryImageView = UIImageView().then {
+        $0.image = ImageLiteral.eventCategory
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        render()
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func render() {
+        
+        self.contentView.addSubview(categoryImageView)
+        
+        categoryImageView.snp.makeConstraints { make in
+            make.size.equalTo(30)
+            make.center.equalToSuperview()
+        }
+    }
+    
+    private func configure() {
+        self.layer.cornerRadius = 20
+        self.backgroundColor = .hous_blue_bg
+    }
+}
