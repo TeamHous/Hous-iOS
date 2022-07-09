cdb0b96c2782232cb55bd67c3c8cfa731218bfa0
Kim Minjae <ffalswo2@naver.com>
2022-07-10 01:46:48 +0900

    [#10] CHORE : 이벤트 데이터모델 수정

--- Hous-iOS/Hous-iOS/Screens/Home/Model/EventsDataModel.swift
+++ Hous-iOS/Hous-iOS/Screens/Home/Model/EventsDataModel.swift
@@ -5,25 +5,26 @@
 //  Created by 김민재 on 2022/07/09.
 //
 
-import Foundation
+import UIKit
 
 struct EventDataModel {
   let dday: Int
   var ddayString: String {
     return "D-\(dday)"
   }
+  let eventImage: UIImage?
 }
 
 extension EventDataModel {
   static let sampleData: [EventDataModel] = [
-    EventDataModel(dday: 1),
-    EventDataModel(dday: 3),
-    EventDataModel(dday: 4),
-    EventDataModel(dday: 11),
-    EventDataModel(dday: 42),
-    EventDataModel(dday: 11),
-    EventDataModel(dday: 1),
-    EventDataModel(dday: 4),
-    EventDataModel(dday: 5)
+    EventDataModel(dday: 1, eventImage: R.Image.beerYellow),
+    EventDataModel(dday: 3, eventImage: R.Image.cakeYellow),
+    EventDataModel(dday: 4, eventImage: R.Image.coffeeYellow),
+    EventDataModel(dday: 11, eventImage: R.Image.partyYellow),
+    EventDataModel(dday: 42, eventImage: R.Image.partyYellow),
+    EventDataModel(dday: 11, eventImage: R.Image.partyYellow),
+    EventDataModel(dday: 1, eventImage: R.Image.coffeeYellow),
+    EventDataModel(dday: 4, eventImage: R.Image.coffeeYellow),
+    EventDataModel(dday: 5, eventImage: R.Image.cakeYellow)
   ]
 }
