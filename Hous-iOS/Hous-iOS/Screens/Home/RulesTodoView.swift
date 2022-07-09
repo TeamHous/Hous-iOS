3310422eaf5371aef6ad92137d75df492e993d4b
Kim Minjae <ffalswo2@naver.com>
2022-07-09 20:12:26 +0900

    [#10] FEAT : Rule, Todo 재사용View 추가

--- Hous-iOS/Hous-iOS/Screens/Home/RulesTodoView.swift
+++ Hous-iOS/Hous-iOS/Screens/Home/RulesTodoView.swift
@@ -7,14 +7,67 @@
 
 import UIKit
 
-class RulesTodoView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+class RulesTodosView: UIView {
+    
+    let circleImageView = UIImageView().then {
+        $0.image = UIImage(systemName: "circle.fill")
+        $0.tintColor = .blue
     }
-    */
-
+    
+    let checkButton = UIButton().then {
+        $0.setImage(UIImage(systemName: "checkmark.square"), for: .normal)
+        $0.setImage(UIImage(systemName: "checkmark.square.fill"), for: .selected)
+        $0.tintColor = UIColor(hex: "5E9EFF")
+    }
+    
+    private let ruleLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 13, weight: .medium)
+//        $0.lineBreakMode = .byWordWrapping
+//        $0.lineBreakStrategy = .hangulWordPriority
+        $0.textAlignment = .left
+        $0.numberOfLines = 1
+    }
+    
+    private lazy var stackView = UIStackView(arrangedSubviews: [circleImageView, checkButton, ruleLabel]).then {
+        $0.axis = .horizontal
+        $0.alignment = .firstBaseline
+        $0.spacing = 8
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        render()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func render() {
+        addSubViews([stackView])
+        circleImageView.snp.makeConstraints {
+            $0.height.width.equalTo(8)
+        }
+        
+        checkButton.snp.makeConstraints {
+            $0.height.width.equalTo(14)
+        }
+        
+        ruleLabel.snp.makeConstraints {
+            $0.leading.equalTo(circleImageView.snp.trailing).offset(8)
+            $0.trailing.equalToSuperview()
+        }
+        
+        stackView.snp.makeConstraints {
+            $0.top.bottom.leading.trailing.equalToSuperview()
+        }
+    }
+    
+    func setRulesLabelData(rule: String) {
+        ruleLabel.text = rule
+    }
+    
+    func setCheckButton(_ isDone: Bool) {
+        checkButton.isSelected = isDone
+    }
 }
