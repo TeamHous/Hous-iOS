210b6b852282469322acd8561dcb78568bdf90e7
Jihyun247 <wlguslove12345@gmail.com>
2022-07-09 15:29:43 +0900

    [#8] FEAT : Rules탭 VC

--- Hous-iOS/Hous-iOS/Screens/Rules/RulesViewController.swift
+++ Hous-iOS/Hous-iOS/Screens/Rules/RulesViewController.swift
@@ -5,5 +5,63 @@
 //  Created by 김지현 on 2022/07/07.
 //
 
-import Foundation
-// 이 파일 삭제하고 자유롭게 폴더 이용하세요!
+import UIKit
+import SnapKit
+import Then
+import RxSwift
+import RxCocoa
+
+final class RulesViewController: UIViewController {
+  
+  let disposeBag = DisposeBag()
+  
+  enum RulesCellType {
+    case todayTodo, myTodo
+  }
+  
+  var rulesCellType: RulesCellType = .myTodo
+  var mainView = RulesHomeView()
+  
+  override func loadView() {
+    self.view = mainView
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setCollectionView()
+  }
+  
+  private func setCollectionView() {
+    mainView.categoryCollectionView.delegate = self
+    mainView.categoryCollectionView.dataSource = self
+    mainView.todoDisplayView.todoCollectionView.delegate = self
+    mainView.todoDisplayView.todoCollectionView.dataSource = self
+  }
+}
+
+extension RulesViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+  
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 8
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    
+    switch collectionView {
+    case mainView.categoryCollectionView:
+      let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CategoryCollectionViewCell.identifier, for: indexPath)
+      return cell
+    case mainView.todoDisplayView.todoCollectionView:
+      switch rulesCellType {
+      case .todayTodo:
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TodayTodoCollectionViewCell.identifier, for: indexPath)
+        return cell
+      case .myTodo:
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MyTodoCollectionViewCell.identifier, for: indexPath)
+        return cell
+      }
+    default:
+      return UICollectionViewCell()
+    }
+  }
+}
