714c464cf85612c341f075141cb7a19f6bd8e32a
Kim Minjae <ffalswo2@naver.com>
2022-07-09 20:05:11 +0900

    [#10] ADD : 메인뷰 구현위한 파일들 추가

---
+++ Hous-iOS/Hous-iOS/Screens/Home/Cell/RuleTodo/RulesTodoCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  RulesTodoCollectionViewCell.swift
+//  Hous-iOS
+//
+//  Created by 김민재 on 2022/07/09.
+//
+
+import UIKit
+
+class RulesTodoCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Hous-iOS/Hous-iOS/Screens/Home/PopUpViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PopUpViewController.swift
+//  Hous-iOS
+//
+//  Created by 김민재 on 2022/07/09.
+//
+
+import UIKit
+
+class PopUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
