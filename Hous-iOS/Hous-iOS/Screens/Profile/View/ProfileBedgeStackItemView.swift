ea5f705747012d7a32d4e18d64d00392e18dc92a
lee-euiji <leeuj9663@naver.com>
2022-07-12 02:35:36 +0900

    [#19] FEAT : Profile Main View Bedge Cell item layout 완료

---
+++ Hous-iOS/Hous-iOS/Screens/Profile/View/ProfileBedgeStackItemView.swift
@@ -0,0 +1,56 @@
+//
+//  ProfileBedgeStackItemView.swift
+//  Hous-iOS
+//
+//  Created by 이의진 on 2022/07/12.
+//
+
+import UIKit
+
+class profileBedgeCollectionItemView : UIView {
+
+  private let circle = CircleView()
+  
+  var bedgeImage = UIImageView().then{
+    $0.image = R.Image.badgeLocked
+  }
+  
+  var bedgeName = UILabel().then{
+    $0.text = "규칙 한걸음"
+    $0.textColor = .black
+    $0.font = .font(.spoqaHanSansNeoMedium, ofSize: 12)
+  }
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    configure()
+    render()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func configure(){
+    self.backgroundColor = .veryLightPinkTwo
+  }
+  
+  private func render(){
+    [circle, bedgeImage, bedgeName].forEach {self.addSubview($0)}
+    
+    circle.snp.makeConstraints {make in
+      make.top.equalToSuperview()
+      make.width.height.equalTo(70)
+    }
+    
+    bedgeImage.snp.makeConstraints {make in
+      make.top.bottom.leading.trailing.equalTo(circle).inset(22)
+    }
+    
+    bedgeName.snp.makeConstraints {make in
+      make.top.equalTo(circle.snp.bottom).offset(12)
+      make.centerY.equalToSuperview()
+    }
+    
+  }
+}
