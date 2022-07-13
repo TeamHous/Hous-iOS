ce9c0cff4e628da132479af90b1a0803549318f8
Kim Minjae <ffalswo2@naver.com>
2022-07-14 02:50:53 +0900

    [#20] CHORE : AssigneeFactory faceImage 추가

--- Hous-iOS/Hous-iOS/Global/Protocol/AssigneeFactory.swift
+++ Hous-iOS/Hous-iOS/Global/Protocol/AssigneeFactory.swift
@@ -12,56 +12,63 @@ enum AssigneeColor: String {
 }
 
 protocol AssigneeProtocol {
-  var image: UIImage { get set }
+  var checkedFaceImage: UIImage { get set }
+  var faceImage: UIImage { get set }
   var color: UIColor { get set }
 }
 
 struct YellowAssignee: AssigneeProtocol {
-  var image = R.Image.faceYellow
+  var checkedFaceImage = R.Image.faceCheckedYellow
+  var faceImage = R.Image.faceYellow
   var color = R.Color.paleGold
 }
 
 struct BlueAssignee: AssigneeProtocol {
-  var image = R.Image.faceBlue
+  var checkedFaceImage = R.Image.faceCheckedBlue
+  var faceImage = R.Image.faceBlue
   var color = R.Color.softBlue
 }
 
 struct PurpleAssignee: AssigneeProtocol {
-  var image = R.Image.facePurple
+  var checkedFaceImage = R.Image.faceCheckedPurple
+  var faceImage = R.Image.facePurple
   var color = R.Color.lilac
 }
 
 struct RedAssignee: AssigneeProtocol {
-  var image = R.Image.faceRed
+  var checkedFaceImage = R.Image.faceCheckedRed
+  var faceImage = R.Image.faceRed
   var color = R.Color.salmon
 }
 
 struct GreenAssignee: AssigneeProtocol {
-  var image = R.Image.faceGreen
+  var checkedFaceImage = R.Image.faceCheckedRed
+  var faceImage = R.Image.faceGreen
   var color = R.Color.easterGreen
 }
 
 struct NoAssignee: AssigneeProtocol {
-  var image = R.Image.assignAdd
+  var checkedFaceImage = R.Image.assignAdd
+  var faceImage = R.Image.assignAdd
   var color = R.Color.veryLightPinkFour
 }
 
 struct AssigneeFactory {
-
-    static func makeAssignee(type: AssigneeColor) -> AssigneeProtocol {
-      switch type {
-      case .yellow:
-        return YellowAssignee()
-      case .blue:
-        return BlueAssignee()
-      case .purple:
-        return PurpleAssignee()
-      case .red:
-        return RedAssignee()
-      case .green:
-        return GreenAssignee()
-      case .none:
-        return NoAssignee()
-      }
+  
+  static func makeAssignee(type: AssigneeColor) -> AssigneeProtocol {
+    switch type {
+    case .yellow:
+      return YellowAssignee()
+    case .blue:
+      return BlueAssignee()
+    case .purple:
+      return PurpleAssignee()
+    case .red:
+      return RedAssignee()
+    case .green:
+      return GreenAssignee()
+    case .none:
+      return NoAssignee()
     }
+  }
 }
