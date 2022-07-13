a11a72979be386f9719349b669c01c01c2fe786c
Jihyun247 <wlguslove12345@gmail.com>
2022-07-14 01:20:40 +0900

    [#27] FEAT : Assignee Factory

---
+++ Hous-iOS/Hous-iOS/Global/Protocol/AssigneeFactory.swift
@@ -0,0 +1,67 @@
+//
+//  AssigneeFactory.swift
+//  Hous-iOS
+//
+//  Created by 김지현 on 2022/07/14.
+//
+
+import UIKit
+
+enum AssigneeColor: String {
+  case yellow, blue, purple, red, green, none
+}
+
+protocol AssigneeProtocol {
+  var image: UIImage { get set }
+  var color: UIColor { get set }
+}
+
+struct YellowAssignee: AssigneeProtocol {
+  var image = R.Image.faceYellow
+  var color = R.Color.paleGold
+}
+
+struct BlueAssignee: AssigneeProtocol {
+  var image = R.Image.faceBlue
+  var color = R.Color.softBlue
+}
+
+struct PurpleAssignee: AssigneeProtocol {
+  var image = R.Image.facePurple
+  var color = R.Color.lilac
+}
+
+struct RedAssignee: AssigneeProtocol {
+  var image = R.Image.faceRed
+  var color = R.Color.salmon
+}
+
+struct GreenAssignee: AssigneeProtocol {
+  var image = R.Image.faceGreen
+  var color = R.Color.easterGreen
+}
+
+struct NoAssignee: AssigneeProtocol {
+  var image = R.Image.assignAdd
+  var color = R.Color.veryLightPinkFour
+}
+
+struct AssigneeFactory {
+
+    static func makeAssignee(type: AssigneeColor) -> AssigneeProtocol {
+      switch type {
+      case .yellow:
+        return YellowAssignee()
+      case .blue:
+        return BlueAssignee()
+      case .purple:
+        return PurpleAssignee()
+      case .red:
+        return RedAssignee()
+      case .green:
+        return GreenAssignee()
+      case .none:
+        return NoAssignee()
+      }
+    }
+}
