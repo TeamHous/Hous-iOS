e9a43589bdbea51f9fc1c582005701e57ceaa1af
Kim Minjae <ffalswo2@naver.com>
2022-07-14 02:48:18 +0900

    [#20] FEAT : IconFactory 구현

--- Hous-iOS/Hous-iOS/Global/Protocol/IconFactory.swift
+++ Hous-iOS/Hous-iOS/Global/Protocol/IconFactory.swift
@@ -5,4 +5,48 @@
 //  Created by 김민재 on 2022/07/14.
 //
 
-import Foundation
+import UIKit
+
+enum IconImage: String {
+  case party, cake, beer, coffee
+}
+
+protocol IconProtocol {
+  var bigIconImage: UIImage { get set }
+  var smallIconImage: UIImage { get set }
+}
+
+struct PartyIcon: IconProtocol {
+  var bigIconImage = R.Image.partyYellow
+  var smallIconImage = R.Image.partyYellowSmall
+}
+
+struct CakeIcon: IconProtocol {
+  var bigIconImage = R.Image.cakeYellow
+  var smallIconImage = R.Image.cakeYellowSmall
+}
+
+struct BeerIcon: IconProtocol {
+  var bigIconImage = R.Image.beerYellow
+  var smallIconImage = R.Image.beerYellowSmall
+}
+
+struct CoffeeIcon: IconProtocol {
+  var bigIconImage = R.Image.coffeeYellow
+  var smallIconImage = R.Image.coffeeYellowSmall
+}
+
+struct IconFactory {
+  static func makeIcon(type: IconImage) -> IconProtocol {
+    switch type {
+    case .party:
+      return PartyIcon()
+    case .coffee:
+      return CoffeeIcon()
+    case .beer:
+      return BeerIcon()
+    case .cake:
+      return CakeIcon()
+    }
+  }
+}
