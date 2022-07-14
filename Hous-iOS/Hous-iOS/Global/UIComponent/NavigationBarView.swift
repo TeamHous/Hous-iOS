5c9e81631e1fe0695578efaea0119c0945686dd5
Jihyun247 <wlguslove12345@gmail.com>
2022-07-14 16:59:02 +0900

    [#23] CHORE : #19 충돌 해결

--- Hous-iOS/Hous-iOS/Global/UIComponent/NavigationBarView.swift
+++ Hous-iOS/Hous-iOS/Global/UIComponent/NavigationBarView.swift
@@ -23,9 +23,17 @@ class NavigationBarView: UIView {
     $0.text = "Hous-"
   }
 
+  private let updateProfileButton = UIButton().then{
+    $0.setImage(R.Image.updateProfile, for: .normal)
+  }
+
+  private let settingProfileButton = UIButton().then{
+    $0.setImage(R.Image.settingProfile, for: .normal)
+  }
+
   convenience init(tabType: TabType) {
     self.init(frame: .zero)
-    render()
+    render(tabType)
     configure(tabType)
   }
 
@@ -37,7 +45,7 @@ class NavigationBarView: UIView {
     fatalError("init(coder:) has not been implemented")
   }
 
-  private func render() {
+  private func render(_ tabType: TabType) {
 
     self.addSubViews([titleLabel])
 
@@ -45,57 +53,25 @@ class NavigationBarView: UIView {
       make.centerY.equalToSuperview().multipliedBy(1.5)
       make.leading.equalToSuperview().inset(20)
     }
-    private let updateProfileButton = UIButton().then{
-      $0.setImage(R.Image.updateProfile, for: .normal)
-    }
-  
-    private let settingProfileButton = UIButton().then{
-      $0.setImage(R.Image.settingProfile, for: .normal)
-  }
-    
-    convenience init(tabType: TabType) {
-      self.init(frame: .zero)
-      render(tabType)
-      configure(tabType)
-    }
-    
-    override init(frame: CGRect) {
-      super.init(frame: frame)
-    }
-    
-    required init?(coder: NSCoder) {
-      fatalError("init(coder:) has not been implemented")
-    }
-    
-  private func render(_ tabType: TabType) {
-        
-      self.addSubViews([titleLabel])
-        
-      titleLabel.snp.makeConstraints { make in
-          make.centerY.equalToSuperview().multipliedBy(1.5)
-          make.leading.equalToSuperview().inset(20)
-      }
-    
+
     if tabType == .profile{
       self.addSubViews([updateProfileButton, settingProfileButton])
-      
+
       updateProfileButton.snp.makeConstraints {make in
         make.trailing.equalToSuperview().offset(-60)
         make.top.equalTo(safeAreaLayoutGuide).offset(21)
         make.width.height.equalTo(24)
       }
-      
+
       settingProfileButton.snp.makeConstraints {make in
         make.trailing.equalToSuperview().offset(-24)
         make.top.equalTo(safeAreaLayoutGuide).offset(21)
         make.width.height.equalTo(24)
       }
     }
-      
-      
-    }
-    
-    private func configure(_ tabType: TabType) {
-      titleLabel.text = tabType.rawValue
-    }
+  }
+
+  private func configure(_ tabType: TabType) {
+    titleLabel.text = tabType.rawValue
+  }
 }
