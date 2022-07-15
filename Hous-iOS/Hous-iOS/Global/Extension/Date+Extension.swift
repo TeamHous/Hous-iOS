fc3651deb4650f380546a2fda1f6a18d9dbad321
Kim Minjae <ffalswo2@naver.com>
2022-07-16 06:24:50 +0900

    [#40] FEAT : String to Date, Date calendar Extension 추가

--- Hous-iOS/Hous-iOS/Global/Extension/Date+Extension.swift
+++ Hous-iOS/Hous-iOS/Global/Extension/Date+Extension.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+extension Date {
+  var year: Int {
+    let calendar = Calendar.current
+    return calendar.component(.year, from: self)
+  }
+  
+  var month: Int {
+    let calendar = Calendar.current
+    return calendar.component(.month, from: self)
+  }
+  
+  var day: Int {
+    let calendar = Calendar.current
+    return calendar.component(.day, from: self)
+  }
+}
--- Hous-iOS/Hous-iOS/Global/Extension/String+Extension.swift
+++ Hous-iOS/Hous-iOS/Global/Extension/String+Extension.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+extension String {
+  func toDate() -> Date? {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyy-MM-dd"
+    dateFormatter.timeZone = TimeZone(identifier: "UTC")
+    
+    if let date = dateFormatter.date(from: self) {
+      return date
+    } else {
+      return nil
+    }
+  }
+}
