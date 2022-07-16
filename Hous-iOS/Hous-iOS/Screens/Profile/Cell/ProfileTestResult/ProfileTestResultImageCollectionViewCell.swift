00b7864797dc949a370ce2b92a467c8e7c89c287
lee-euiji <leeuj9663@naver.com>
2022-07-17 03:56:44 +0900

    [#50] FEAT : 결과 뷰 이미지 셀 완료

--- Hous-iOS/Hous-iOS/Screens/Profile/Cell/ProfileTestResult/ProfileTestResultImageCollectionViewCell.swift
+++ Hous-iOS/Hous-iOS/Screens/Profile/Cell/ProfileTestResult/ProfileTestResultImageCollectionViewCell.swift
@@ -9,4 +9,61 @@ import UIKit
 
 class ProfileTestResultImageCollectionViewCell : UICollectionViewCell {
   
+  private enum Size{
+    static let screenWidth = UIScreen.main.bounds.width
+    static let screenHeight = UIScreen.main.bounds.height
+    static let imageSize = CGSize(width: 300, height: 300)
+  }
+  
+  private let userNameLabel = UILabel().then {
+    $0.text = "최호미님은"
+    $0.textColor = .brownGrey
+    $0.backgroundColor = .white
+    $0.font = .font(.spoqaHanSansNeoMedium, ofSize: 16)
+  }
+  
+  private let personalityTypeLabel = UILabel().then {
+    $0.text = "늘 행복한 동글이"
+    $0.textColor = .housBlack
+    $0.backgroundColor = .white
+    $0.font = .font(.spoqaHanSansNeoBold, ofSize: 22)
+  }
+  
+  private let personalityImageView = UIImageView().then {
+    $0.image = R.Image.resultImage1
+  }
+  
+  override init(frame: CGRect){
+    super.init(frame: frame)
+    configUI()
+    render()
+  }
+  
+  required init?(coder: NSCoder){
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func configUI(){
+    self.backgroundColor = .white
+  }
+  
+  private func render(){
+    self.addSubViews([userNameLabel, personalityTypeLabel, personalityImageView])
+    
+    userNameLabel.snp.makeConstraints {make in
+      make.centerX.equalToSuperview()
+      make.top.equalToSuperview()
+    }
+    
+    personalityTypeLabel.snp.makeConstraints {make in
+      make.centerX.equalToSuperview()
+      make.top.equalTo(userNameLabel.snp.bottom).offset(2)
+    }
+    
+    personalityImageView.snp.makeConstraints {make in
+      make.centerX.equalTo(personalityTypeLabel)
+      make.top.equalTo(personalityTypeLabel.snp.bottom).offset(9)
+      make.width.height.equalTo(Size.imageSize)
+    }
+  }
 }
--- Hous-iOS/Hous-iOS/Screens/Profile/Cell/ProfileTestResult/ProfileTestResultTextCollectionViewCell.swift
+++ Hous-iOS/Hous-iOS/Screens/Profile/Cell/ProfileTestResult/ProfileTestResultTextCollectionViewCell.swift
@@ -9,4 +9,5 @@ import UIKit
 
 class ProfileTestResultTextCollectionViewCell : UICollectionViewCell {
   
+  
 }
