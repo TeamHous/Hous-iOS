48041007971133c5934d62bfd98b89adc8052ed7
lee-euijin <leeuj9663@naver.com>
2022-07-17 06:10:06 +0900

    [#50] FIX : NavigationBarView 프로필 버튼 위치 수정

--- Hous-iOS/Hous-iOS/Global/UIComponent/NavigationBarView.swift
+++ Hous-iOS/Hous-iOS/Global/UIComponent/NavigationBarView.swift
@@ -19,32 +19,32 @@ class NavigationBarView: UIView {
   
   var moveToSettingViewController : (() -> Void)?
   var moveToEditingViewController : (() -> Void)?
-
+  
   private var titleLabel = UILabel().then {
     $0.textColor = R.Color.housBlack
     $0.font = .font(.montserratSemiBold, ofSize: 26)
     $0.text = "Hous-"
   }
-
+  
   private let editingProfileButton = UIButton().then{
     $0.setImage(R.Image.updateProfile, for: .normal)
   }
-
+  
   private let settingProfileButton = UIButton().then{
     $0.setImage(R.Image.settingProfile, for: .normal)
   }
-
+  
   convenience init(tabType: TabType) {
     self.init(frame: .zero)
     render(tabType)
     configure(tabType)
     setup()
   }
-
+  
   override init(frame: CGRect) {
     super.init(frame: frame)
   }
-
+  
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
@@ -53,32 +53,32 @@ class NavigationBarView: UIView {
     settingProfileButton.addTarget(self, action: #selector(tabSettingProfileButton), for: .touchUpInside)
     editingProfileButton.addTarget(self, action: #selector(tabEditingProfileButton), for: .touchUpInside)
   }
-
+  
   private func render(_ tabType: TabType) {
-
+    
     self.addSubViews([titleLabel])
     titleLabel.snp.makeConstraints { make in
       make.centerY.equalToSuperview().multipliedBy(1.5)
       make.leading.equalToSuperview().inset(24)
     }
-
+    
     if tabType == .profile{
       self.addSubViews([editingProfileButton, settingProfileButton])
-
+      
       editingProfileButton.snp.makeConstraints {make in
         make.trailing.equalToSuperview().offset(-60)
-        make.top.equalTo(safeAreaLayoutGuide).multipliedBy(1.5)
+        make.centerY.equalToSuperview().multipliedBy(1.5)
         make.width.height.equalTo(24)
       }
-
+      
       settingProfileButton.snp.makeConstraints {make in
         make.trailing.equalToSuperview().offset(-24)
-        make.top.equalTo(safeAreaLayoutGuide).multipliedBy(1.5)
+        make.centerY.equalToSuperview().multipliedBy(1.5)
         make.width.height.equalTo(24)
       }
     }
   }
-
+  
   private func configure(_ tabType: TabType) {
     titleLabel.text = tabType.rawValue
   }
