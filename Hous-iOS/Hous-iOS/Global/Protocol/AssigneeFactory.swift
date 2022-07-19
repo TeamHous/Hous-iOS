23ac34079b1828c376a57e362006472b1e24772e
Kim Minjae <ffalswo2@naver.com>
2022-07-20 03:15:13 +0900

    [#78] FEAT : AssigneeFactory 프로필 background 추가

--- Hous-iOS/Hous-iOS/Global/Protocol/AssigneeFactory.swift
+++ Hous-iOS/Hous-iOS/Global/Protocol/AssigneeFactory.swift
@@ -15,48 +15,56 @@ protocol AssigneeProtocol {
   var checkedFaceImage: UIImage { get set }
   var faceImage: UIImage { get set }
   var color: UIColor { get set }
+  var profileBackgroundColor: UIColor { get set }
 }
 
 struct YellowAssignee: AssigneeProtocol {
   var checkedFaceImage = R.Image.faceCheckedYellow
   var faceImage = R.Image.faceYellow
   var color = R.Color.paleGold
+  var profileBackgroundColor = R.Color.offWhite
 }
 
 struct BlueAssignee: AssigneeProtocol {
   var checkedFaceImage = R.Image.faceCheckedBlue
   var faceImage = R.Image.faceBlue
   var color = R.Color.softBlue
+  var profileBackgroundColor = R.Color.paleGrey
 }
 
 struct PurpleAssignee: AssigneeProtocol {
   var checkedFaceImage = R.Image.faceCheckedPurple
   var faceImage = R.Image.facePurple
   var color = R.Color.lilac
+  var profileBackgroundColor = R.Color.paleGreyTwo
 }
 
 struct RedAssignee: AssigneeProtocol {
   var checkedFaceImage = R.Image.faceCheckedRed
   var faceImage = R.Image.faceRed
   var color = R.Color.salmon
+  var profileBackgroundColor = R.Color.veryLightPinkTwo
 }
 
 struct GreenAssignee: AssigneeProtocol {
   var checkedFaceImage = R.Image.faceCheckedRed
   var faceImage = R.Image.faceGreen
   var color = R.Color.easterGreen
+  var profileBackgroundColor = R.Color.blueMint
 }
 
 struct GrayAssignee: AssigneeProtocol {
-  var checkedFaceImage = R.Image.faceCheckedRed
+  var checkedFaceImage = R.Image.faceCheckedGray
   var faceImage = R.Image.faceEmpty
   var color = R.Color.veryLightPinkFour
+  var profileBackgroundColor = R.Color.veryLightPinkThree
 }
 
 struct NoAssignee: AssigneeProtocol {
   var checkedFaceImage = R.Image.faceCheckedGray
   var faceImage = R.Image.faceEmpty
   var color = R.Color.lightPeriwinkle
+  var profileBackgroundColor = R.Color.veryLightPinkThree
 }
 
 struct AssigneeFactory {
