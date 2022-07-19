86395d1985b727741fc7281e29bcffe1f7439587
Jihyun247 <wlguslove12345@gmail.com>
2022-07-19 23:05:44 +0900

    [#68] FEAT : plus button 추가

---
+++ Hous-iOS/Hous-iOS/Global/UIComponent/PlusButton.swift
@@ -0,0 +1,46 @@
+//
+//  PlusButton.swift
+//  Hous-iOS
+//
+//  Created by 김호세 on 2022/07/11.
+//
+
+import UIKit
+
+import SnapKit
+
+final class PlusButton: UIControl {
+
+  private let plusImageView: UIImageView = {
+    let imageView = UIImageView()
+    imageView.image = UIImage(
+      systemName: "plus",
+      withConfiguration: UIImage.SymbolConfiguration(
+        pointSize: 16,
+        weight: .bold
+      )
+    )
+    imageView.backgroundColor = .white
+    return imageView
+  }()
+
+  init() {
+    super.init(frame: .zero)
+    setupViews()
+  }
+  required init?(coder: NSCoder) {
+    super.init(coder: coder)
+    setupViews()
+  }
+
+  private func setupViews() {
+    backgroundColor = .white
+    layer.cornerCurve = .continuous
+    layer.cornerRadius = 16
+    addSubview(plusImageView)
+
+    plusImageView.snp.makeConstraints { make in
+      make.center.equalToSuperview()
+    }
+  }
+}
