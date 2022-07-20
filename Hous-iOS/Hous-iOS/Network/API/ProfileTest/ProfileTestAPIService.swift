0dd18d3647424517c0e2b5168184b607adc5d419
Kim Minjae <ffalswo2@naver.com>
2022-07-20 19:34:22 +0900

    [#82] ADD : ProfileTest API 관련 파일들 추가

---
+++ Hous-iOS/Hous-iOS/Network/API/ProfileTest/ProfileTestAPIService.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileTestAPIService.swift
+//  Hous-iOS
+//
+//  Created by 김민재 on 2022/07/20.
+//
+
+import Foundation
---
+++ Hous-iOS/Hous-iOS/Network/API/ProfileTest/ProfileTestAPITarget.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileTestAPITarget.swift
+//  Hous-iOS
+//
+//  Created by 김민재 on 2022/07/20.
+//
+
+import Foundation
