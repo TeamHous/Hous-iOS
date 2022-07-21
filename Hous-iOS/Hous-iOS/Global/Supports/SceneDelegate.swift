14324175b466e66730c8c6a4aaffaa61ab726a56
Kim Minjae <ffalswo2@naver.com>
2022-07-22 01:14:18 +0900

    [#103] FEAT : 다크모드 막기

--- Hous-iOS/Hous-iOS/Global/Supports/SceneDelegate.swift
+++ Hous-iOS/Hous-iOS/Global/Supports/SceneDelegate.swift
@@ -18,7 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     guard let windowScene = (scene as? UIWindowScene) else { return }
     let window = UIWindow(windowScene: windowScene)
     window.backgroundColor = .white
-
+    window.overrideUserInterfaceStyle = .light
     window.rootViewController = SplashViewController()
     window.makeKeyAndVisible()
     self.window = window
